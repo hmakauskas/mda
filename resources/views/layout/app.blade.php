246c8217c14d54799c4b4c7f77e298379c1e81dc
Henrique Makauskas <henrique.makauskas@dafiti.local>
2015-12-12 13:25:18 -0200

    logo

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -40,8 +40,12 @@
 
 <body>
 	<div class="container">
+		
 		<nav class="navbar navbar-default">
+		  
 		  <div class="container-fluid">
+
+
 		    <!-- Brand and toggle get grouped for better mobile display -->
 		    <div class="navbar-header">
 		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -50,7 +54,7 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span>
 		      </button>
-		      <a class="navbar-brand" href="<?=url('/')?>">Madruga</a>
+		      <a class="navbar-brand" href="<?=url('/')?>" style="padding:2px;"><img src="<?=url('/')?>/images/logo.png"/></a>
 		    </div>
 
 		    <!-- Collect the nav links, forms, and other content for toggling -->
@@ -82,9 +86,10 @@
 		    </div><!-- /.navbar-collapse -->
 		  </div><!-- /.container-fluid -->
 		</nav>
-	</div>
+	
+		@yield('content')	
 
-	@yield('content')
+	</div>
 </body>
 
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
